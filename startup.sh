f4d74296b99b46b9b85412d212bd060c78a996b9
Jeff Wozniak <jwozniak@SB-JWOZNIAK.local>
2016-10-28 17:31:13 -0700

    Improve signal handling in wrapper script

--- startup.sh
+++ startup.sh
@@ -1,3 +1,26 @@
-#!/bin/bash
-python /startup.py
-tail -f /usr/local/logicmonitor/agent/logs/wrapper.log
+#!/usr/bin/env bash
+
+signal_handler() {
+  echo $pid
+  if [ ! -z $pid ]; then
+    kill -s 15 $pid
+    python /shutdown.py
+    exit 0
+  fi
+}
+
+# setup handlers
+trap 'kill ${!}; signal_handler' SIGTERM
+trap 'kill ${!}; signal_handler' SIGINT
+
+# run application
+python /startup.py &
+pid="$!"
+
+# wait forever
+while true
+do
+  if [ -e "/usr/local/logicmonitor/agent/logs/wrapper.log" ]; then
+    tail -f /usr/local/logicmonitor/agent/logs/wrapper.log & wait
+  fi
+done
