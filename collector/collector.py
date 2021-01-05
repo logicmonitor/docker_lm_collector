073cf9bd590de0198bcb7434a36af4206848d031
Vrushali Talele <vrushali.talele@logicmonitor.com>
2021-01-05 15:26:21 +0530

    update

--- collector/collector.py
+++ collector/collector.py
@@ -206,7 +206,7 @@ def download_installer(client, collector, params):
     }
     if not kwargs['use_ea']:
         if 'extra_large' in kwargs['collector_size'] or 'double_extra_large' in kwargs['collector_size']:
-            err = 'Cannot proceed with installation because only Early Access collector versions support ' + kwargs['collector_size'] + 'size. To process further with installation, set \"use_ea\" parameter to true or use appropriate collector size.\n'
+            err = 'Cannot proceed with installation because only Early Access collector versions support ' + kwargs['collector_size'] + 'size. To proceed further with installation, set \"use_ea\" parameter to true or use appropriate collector size.\n'
             util.fail(err)
 
     if 'collector_version' in params and params['collector_version']:
