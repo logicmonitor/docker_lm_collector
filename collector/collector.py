0d43f7156cad32297f7e1839157ac03a9358a5df
vkumbhar94 <vkumbhar94@gmail.com>
2022-01-13 18:29:12 +0530

    release-vkubuntu20.04:

--- collector/collector.py
+++ collector/collector.py
@@ -285,10 +285,13 @@ def install_collector(client, collector, params):
 
     result = util.shell(install_cmd)
     if result['code'] != 0 or result['stderr'] != '':
-        err = result['stderr']
+        logging.DEBUG("Collector install script output: " + str(err))
+        err = str(result['stderr'])
+        # err = result['stderr']
         # if we failed but there's no stderr, set err msg to stdout
         if err == '':
-            err = result['stdout']
+            err = str(result['stdout'])
+        err.encode('utf-8')
 
         # check for false fail condition
         success_msg = 'LogicMonitor Collector has been installed successfully'
